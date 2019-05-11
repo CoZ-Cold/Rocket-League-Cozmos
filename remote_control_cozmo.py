781454053de36e0096aab3f97aa3a49c364c589f
Mitchell J Cumming <mitchelljc2718@gmail.com>
2019-05-10 19:40:56 -0500

    Update remote_control_cozmo.py

--- remote_control_cozmo.py
+++ remote_control_cozmo.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2016 Anki, Inc.
+# Heavily modified code barrowed from anki.
+# Added wlecome images and optimized for cozmo rocket leageue .   MITCHELL CUMMING
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License in the file LICENSE.txt or at
+#
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
